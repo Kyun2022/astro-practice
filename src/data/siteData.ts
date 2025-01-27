0f2876e7baebe386e8a577ecfe754741df8833f3
kyun2022 <fm13071303@rx.tnc.ne.jp>
2025-01-27 14:03:15 +0900

    ✨

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: "Kitie Haus Portfolio site",
+  description:
+    "Welcome to Kitie Haus Portfolio site! I'm a self-taught designer and developer obsessed with quality.",
+};
